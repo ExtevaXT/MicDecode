8d0b57834eb0fd177badc9783986948ce4deff08
ExtevaXT <96924741+ExtevaXT@users.noreply.github.com>
2022-05-28 11:01:59 +0500

    Added directories support

--- MicDecode/Program.cs
+++ MicDecode/Program.cs
@@ -11,25 +11,48 @@ namespace MicDecode
     {
         static void Main(string[] args)
         {
-            string inputDir = @"C:\input\";
-            string outputDir = @"C:\output\";
+            string inputDirDefault = @"C:\input\";
+            string outputDirDefault = @"C:\output\";
 
             Console.WriteLine("INPUT");
-            inputDir = Console.ReadLine();
+            string inputDir = Console.ReadLine();
             Console.WriteLine("OUTPUT");
-            outputDir = Console.ReadLine();
-            if (inputDir == null || outputDir == null) return;
-            var files = Directory.GetFiles(inputDir, "*.mic");
-            foreach (string currentFile in files)
+            string outputDir = Console.ReadLine();
+            if (inputDir == null || inputDir == "") inputDir = inputDirDefault;
+            if (outputDir == null || outputDir == "") outputDir = outputDirDefault;
+
+
+            //var files = Directory.GetFiles(inputDir, "*.mic");
+            //foreach (string currentFile in files)
+            //{
+
+            //    FileInfo f1 = new FileInfo(currentFile);
+
+            //    string text = File.ReadAllText(f1.FullName, Encoding.GetEncoding(1252));
+            //    text = text.Replace("MIC", "PNG");
+            //    File.WriteAllText(string.Format("{0}{1}{2}", outputDir, f1.Name.Substring(0, f1.Name.Length - 4), ".png"), text, Encoding.GetEncoding(1252));
+
+            //}
+
+            var inputDirPaths = Directory.GetDirectories(inputDir, "*", SearchOption.AllDirectories);
+
+            foreach (string inputDirPath in inputDirPaths)
             {
+                var files = Directory.GetFiles(inputDirPath, "*.mic");
+                var outputDirPath = outputDir + Path.GetFileName(inputDirPath)+"\\";
+                Directory.CreateDirectory(outputDirPath);
+                foreach (string currentFile in files)
+                {
 
-                FileInfo f1 = new FileInfo(currentFile);
+                    FileInfo f1 = new FileInfo(currentFile);
 
-                string text = File.ReadAllText(f1.FullName, Encoding.GetEncoding(1252));
-                text = text.Replace("MIC", "PNG");
-                File.WriteAllText(string.Format("{0}{1}{2}", outputDir, f1.Name.Substring(0, f1.Name.Length - 4), ".png"), text, Encoding.GetEncoding(1252));
+                    string text = File.ReadAllText(f1.FullName, Encoding.GetEncoding(1252));
+                    text = text.Replace("MIC", "PNG");
+                    File.WriteAllText(string.Format("{0}{1}{2}", outputDirPath, f1.Name.Substring(0, f1.Name.Length - 4), ".png"), text, Encoding.GetEncoding(1252));
 
+                }
             }
         }
+
     }
 }
---
+++ README.md
@@ -0,0 +1,9 @@
+<h1>MicDecoder</h1>
+<p>Easy console app to automatically convert .mic files to .png from stalcraft</p>
+<p>Changes %MIC to %PNG in files and their extension</p>
+<p>Write input and output path in console ("/" in end), default is C:/input/, C:/output/</p>
+<h2>Versions</h2>
+<ul>
+    <li><b>1.0</b> - Base functionality</li>
+    <li><b>1.1</b> - Added foreaching for folders</li>
+</ul>
